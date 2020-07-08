c44f4b7a81c3e97fbf1291c0a21347f562af3c8c
quixoticprogrammer <kvimalraj0611@gmail.com>
2020-07-08 22:50:18 +0530

    created navigation bar for guest and user

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   get '/explore' => 'pages#explore'
   get '/settings' => 'pages#settings'
   get '/help' => 'pages#help'
+  get '/users/help' => 'pages#help'
+  get '/tweet' => 'pages#tweet'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
